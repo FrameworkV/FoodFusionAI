23711cbbbbdba02a7569f8a4c3b1bc4dbcb0fd30
Viet Cuong Ngo <vietcuongngo02vcn@gmail.com>
2024-09-26 17:24:09 +0200

    updated database functions

--- src/backend/database/database.py
+++ src/backend/database/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import bcrypt
 
 from src.backend.models.user import User
 from src.backend.models.groceries import Groceries
@@ -13,9 +14,10 @@ class DatabaseHelper:
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
 
-        cursor.execute(""" CREATE TABLE IF NOT EXISTS users (
+        cursor.execute(""" CREATE TABLE IF NOT EXISTS users(
                                 id integer PRIMARY KEY,
-                                name text
+                                name text,
+                                hashed_password text
                                ); """)
 
         cursor.execute(""" CREATE TABLE IF NOT EXISTS storage(
@@ -33,7 +35,7 @@ class DatabaseHelper:
     def get_conn(self):
         return sqlite3.connect("users.db")
 
-    def insert_user(self, user: User) -> int:
+    def add_user(self, username: str, hashed_password: str) -> int:
         '''
 
         '''
@@ -42,13 +44,12 @@ class DatabaseHelper:
             conn = self.get_conn()
             with conn:
                 cursor = conn.cursor()
-                name = user.get_name()
-                data = cursor.execute("SELECT * FROM users WHERE name = ?", (name,)).fetchone()
+                data = cursor.execute("SELECT * FROM users WHERE name = ?", (username,)).fetchone()
 
                 if data is None:
                     cursor.execute(""" INSERT INTO users
-                                       (name)
-                                       VALUES (?); """, (name,))
+                                       (name, hashed_password)
+                                       VALUES (?, ?); """, (username, hashed_password))
                     user_id = cursor.lastrowid
                     return user_id
                 else:
@@ -62,8 +63,32 @@ class DatabaseHelper:
             cursor.close()
             conn.close()
 
-    def get_user(self,) -> User:
-        pass
+
+    def get_user(self, username: str) -> User:
+        '''
+        
+        '''
+        try:
+            with self.get_conn() as conn:
+                cursor = conn.cursor()
+                user_id = cursor.execute('SELECT id FROM users WHERE name = ?', (username,)).fetchone()[0]
+
+                if user_id is None:
+                    raise ValueError(f"no user found with name: '{username}'")
+                
+                cursor.execute('SELECT name FROM storage WHERE user_id = ?', (user_id))
+                rows = cursor.fetchall()
+                groceries_names = [row[0] for row in rows]
+
+            user = User(name=username, groceries=groceries_names)
+            return user
+        
+        except sqlite3.Error as error:
+            print("Failed to return user from db:", error)
+
+        finally:
+            cursor.close()
+            conn.close()
 
     def delete_user(self, user: User) -> int:
         pass
@@ -71,4 +96,20 @@ class DatabaseHelper:
     def insert_groceries_for_user(self, groceries: Groceries) -> int:
         pass
 
+    def valid_password(self, username: str, password: str) -> bool:
+        try:
+            with self.get_conn() as conn:
+                cursor = conn.cursor()
+                hashed_password = cursor.execute('SELECT hashed_password FROM users WHERE name = ?', (username)).fetchone()[0]
 
+                if hashed_password is None:
+                    raise ValueError(f"no user registred with name: '{username}'")
+                
+            return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+        
+        except sqlite3.Error as error:
+            print("Failed to return hashed_password from db:", error)
+
+        finally:
+            cursor.close()
+            conn.close()
