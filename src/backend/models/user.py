a83110772f1e1299a775bbadf39621448032db6f
Jan Skowron <jan.a.skowron@gmail.com>
2024-10-11 15:08:22 +0200

    make email unique

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -3,10 +3,10 @@ from sqlmodel import SQLModel, Field, Relationship
 from pydantic import EmailStr
 
 class User(SQLModel, table=True):  # User for the database table
-    id: Optional[int] = Field(default=None, primary_key=True)  # automatically assign user id
+    id: Optional[int] = Field(index=True, default=None, primary_key=True)  # automatically assign user id
     username: str = Field(index=True, unique=True)
     password: str
-    email: EmailStr
+    email: EmailStr = Field(index=True, unique=True)
     is_verified: bool = False
 
     groceries: List["Item"] = Relationship(back_populates="user")
