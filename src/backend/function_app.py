4114097e99b84942d2fdfe0894d637b30e8e537d
Jan Skowron <jan.a.skowron@gmail.com>
2024-11-16 17:47:03 +0100

    adjustments for azure functions

--- src/backend/function_app.py
+++ src/backend/function_app.py
@@ -1,4 +1,4 @@
 import azure.functions as func
-from src.backend.app import app as fastapi_app
+from app import app as fastapi_app
 
 app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
