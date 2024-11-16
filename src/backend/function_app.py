76393715c9201f2405dc717566370952f890211f
Jan Skowron <jan.a.skowron@gmail.com>
2024-11-16 15:45:50 +0100

    adjustments for azure functions

--- src/backend/function_app.py
+++
@@ -1,5 +0,0 @@
-import azure.functions as func
-from backend.app import app
-
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return await func.AsgiMiddleware(app).handle_async(req, context)
